fb3f6c4277cd7c2d6f781c823aa5cdfaefa817ee
alisha4mbar <alisha4mbarani.29@student.uns.ac.idm>
2026-04-27 23:05:39 +0700

    update cukkk

--- api/config.php
+++ api/config.php
@@ -1,5 +1,4 @@
 <?php
-// api/config.php
 $host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
 $port = 4000;
 $user = '3woL5zdDuZqqmHS.root';
@@ -7,7 +6,8 @@ $pass = 'IwNMm3ddVFwKzDrf';
 $dbname = 'db_panenusa';
 
 $conn = mysqli_init();
-$ssl_cert = __DIR__ . "/../isrgrootx1.pem";
+// Pastikan file isrgrootx1.pem ada di folder utama (luar API)
+$ssl_cert = __DIR__ . "/../isrgrootx1.pem"; 
 mysqli_ssl_set($conn, NULL, NULL, $ssl_cert, NULL, NULL);
 
 if (!$conn->real_connect($host, $user, $pass, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
