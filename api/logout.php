f4a0640e58b9ea360d37bf87cbd1177bc1a61695
ALISHA AMBARANI <alisha4mbarani.29@student.uns.ac.id>
2026-07-01 00:38:32 +0700

    Delete api/logout.php

--- api/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-setcookie('panenusa_auth', '', time() - 3600, '/'); // Hapus cookie agar tidak bisa re-login otomatis
-header("Location: /"); 
-exit();
-?>
